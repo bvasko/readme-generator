71d49c2f8e4480710e86a96083d6321146ba43f2
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2022-01-05 21:43:28 -0500

    add questions

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'Enter your github user name',
+    name: 'username'
+  },
+  {
+    type: 'input',
+    message: 'Enter the project name:',
+    name: 'projectName'
+  },
+  {
+    type: 'input',
+    message: 'Enter a description',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    message: 'Enter installation instructions',
+    name: 'install'
+  },
+  {
+    type: 'input',
+    message: 'Enter usage information',
+    name: 'usage'
+  },
+  {
+    type: 'input',
+    message: 'Enter contribution information',
+    name: 'contribute'
+  },
+  {
+    type: 'input',
+    message: 'Test instructions',
+    name: 'test'
+  },
+  {
+    type: 'list',
+    choices: ['Apache','BSD','MIT','GNU GPL', 'Eclipse Public License'],
+    message: 'Choose a license',
+    name: 'license'
+  },
+  type: 'input',
+  message: 'Enter contact email',
+  name: 'email'
+];
 
 inquirer
-  .prompt()
+  .prompt(questions)
   .then((response) =>
-    
+    console.log(response);
   );
